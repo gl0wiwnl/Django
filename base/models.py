86649925b8c4c3625c05a68590051634b443a40a
gl0wiwnl <gabriel.valente.567@gmail.com>
2025-01-22 12:24:40 -0400

    more models

--- base/models.py
+++ base/models.py
@@ -1,18 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Topic(models.Model):
+  name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
+
+
+## model Room
+
 class Room(models.Model):
   name = models.CharField(max_length=200)
   description = models.TextField(null=True, blank=True)
   updated = models.DateTimeField(auto_now=True)
   created = models.DateTimeField(auto_now_add=True)
-  #host = 
-  #topic = 
+  host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
   #participants = 
 
   def __str__(self):
-    return str(self.name)
+    return str(self.name)  ## serve para retornar o nome da sala ao instanciar no banco de dados ao inves de retornar o objeto, retornara a string
   
 
-  ## serve para retornar o nome da sala ao instanciar no banco de dados
-  # ao inves de retornar o objeto, retornara a string
+ 
+
+class Message(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE) 
+  room = models.ForeignKey(Room, on_delete=models.CASCADE)
+  body = models.TextField()
+  updated = models.DateTimeField(auto_now=True)
+  created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self): 
+      return str(self.body[0:50])
